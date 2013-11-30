b4d7f29d8a10d08bac17317d6ac60a054dc27994
Fernando Campos <fmcamposrj@gmail.com>
2013-11-30 21:20:49 -0200

    Added Amazon S3 keys to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,13 @@ Pinteresting::Application.configure do
   # Required by Heroku
   config.action_mailer.default_url_options = { :host => 'http://entaum-pinteresting.herokuapp.com/' }
 
+  # Sets paperclip to work with Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
